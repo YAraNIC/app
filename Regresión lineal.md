8ea4832c865cd555f0be3fdeded7d1e852beef77
YAraNIC <yaramartinezmaria1002@gmail.com>
2026-03-25 18:19:32 -0500

    Add files via upload

---
+++ Regresión lineal.md
@@ -0,0 +1,27 @@
+# Regresión lineal
+
+
+Machine learning supervisado
+
+Encuentra la mejor linea que se ajuste a los datos
+
+
+
+Ecuacion
+
+y=ax+b
+
+y= Variable dependiente
+
+
+
+Ecuación para regresión lineal multiple
+
+
+
+
+
+
+
+# Regresión Logistica
+
--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask,render_template, request
+import LinealRegression
 
-app =Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Flask"
+    return  "Hello Flask"
 
 @app.route('/FirstPage')
-def firstPage():
-    return render_template('index.html')
+def firstPage(name = ''):
+    return render_template('index.html', name = name)
+
+
+@app.route('/LinearRegression/', methods=["GET","POST"])
+def calculateGrade():
+    calculateResult=None
+    if request.method == "POST":
+
+        hours = float(request.form["hours"])
+
+        calculateResult= LinealRegression.calculateGrade(hours)
+
+    return render_template("LinearRegressionGrades.html", result = calculateResult)
